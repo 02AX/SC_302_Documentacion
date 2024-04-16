c92a171a6a8e2a924bbe736e5061c2198876795e
Alexander Montero <alexmon2802@gmail.com>
2024-04-15 22:37:22 -0600

    Cambios en el archivo

--- mi-archivo/src/main/java/com/mycompany/mi/archivo/MiArchivo.java
+++ mi-archivo/src/main/java/com/mycompany/mi/archivo/MiArchivo.java
@@ -3,7 +3,7 @@ package com.mycompany.mi.archivo;
 public class MiArchivo {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola mundo_editado");
     
     
     }
