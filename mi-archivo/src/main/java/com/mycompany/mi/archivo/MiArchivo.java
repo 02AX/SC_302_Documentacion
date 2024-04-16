b845da7afeaf164486fcc92e27fde7cc3ecf3b87
Alexander Montero <alexmon2802@gmail.com>
2024-04-15 22:34:37 -0600

    Archivo Hola Mundo

---
+++ mi-archivo/src/main/java/com/mycompany/mi/archivo/MiArchivo.java
@@ -0,0 +1,10 @@
+package com.mycompany.mi.archivo;
+
+public class MiArchivo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    
+    
+    }
+}//Fin de la clase
